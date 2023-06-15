0de576e9c9e94fe29d991bfeca29af6bec1b3b3e
Krooper <98321262+Krooper@users.noreply.github.com>
2023-06-15 05:31:11 +0300

    made 1st task

--- 1/1/untitled/src/Main.java
+++ 1/1/untitled/src/Main.java
@@ -1,5 +1,47 @@
+/*
+Реализуйте 3 метода, чтобы в каждом из них получить разные исключения
+ */
+
+import java.util.Arrays;
+
 public class Main {
+    public static int[] ArrIdxExc(int[] arr) {
+        for (int i = 0; i <= 5; i++) {
+            try {
+                arr[i] = i + 1;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Out of Array!");
+            }
+        }
+        return arr;
+    }
+
+    public static int FormatExc(String myStr, int intVar) {
+        try {
+            intVar = new Integer(myStr);
+        } catch (NumberFormatException e) {
+            System.out.println("Wrong input String! Var unchanged!");
+        }
+        return intVar;
+    }
+
+    public static int[] ArrSize(int arrSize) {
+        try {
+            return new int[arrSize];
+        } catch (NegativeArraySizeException e) {
+            System.out.println("Negative array size!");
+            return new int[0];
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] newArr = new int[5];
+        System.out.println(Arrays.toString(ArrIdxExc(newArr)));
+
+        int newVar = 0;
+        newVar = FormatExc("str", newVar);
+        System.out.println(newVar);
+
+        System.out.println(Arrays.toString(ArrSize(-1)));
     }
 }
