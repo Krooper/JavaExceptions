2ca38aec47050ad7ffbe5589f99cf9063d63ce1f
Krooper <98321262+Krooper@users.noreply.github.com>
2023-06-15 09:14:08 +0300

    made 2nd task

--- 2/_2/src/Main.java
+++ 2/_2/src/Main.java
@@ -6,7 +6,14 @@ public class Main {
     public static void main(String[] args) {
         try {
             int d = 0;
-            double catchedRes1 = intArray[8] / d;
+            /*
+            Не очень точно понял, что именно тут надо исправлять:
+            массив делить на число нельзя, при этом ловится деление на 0.
+            Возможно, подразумевалось деление каждого элемента массива на d?
+            В итоге просто оставил число вместо попытки создания массива.
+            Деление на 0 ловится (для этого надо было еще поменять тип с double на int).
+             */
+            int catchedRes1 = 8 / d;
             System.out.println("catchedRes1 = " + catchedRes1);
         } catch (ArithmeticException e) {
             System.out.println("Catching exception: " + e);
