74be851b238ae71cda751586c7419c708454c973
Krooper <98321262+Krooper@users.noreply.github.com>
2023-06-15 09:32:50 +0300

    made 3rd task

--- 2/_3/src/Main.java
+++ 2/_3/src/Main.java
@@ -1,27 +1,53 @@
+import java.util.Arrays;
+
+
 /*
 Дан следующий код, исправьте его там, где требуется
- */
+*/
 
 
+/*
+Убрал отлов исключений, которых не было в коде.
+Разнес всё по отдельным методам.
+*/
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        printDiv(90, 3);
+        printSum(23, 234);
+        ArrOp(new int[4]);
+    }
+
+    // Перенес catch в метод printSum() для улучшения читаемости.
+    public static void printSum(Integer a, Integer b) {
         try {
-            int a = 90;
-            int b = 3;
-            System.out.println(a / b);
-            printSum(23, 234);
-            int[] abc = { 1, 2 };
-            abc[3] = 9;
-        } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так...");
+            System.out.println(a + b);
         } catch (NullPointerException ex) {
             System.out.println("Указатель не может указывать на null!");
+        }
+    }
+
+    // Добавил метод для деления со своей обработкой исключения.
+    public static void printDiv(Integer a, Integer b) {
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException ex) {
+            System.out.println("Деление на 0!");
+        }
+    }
+
+    /*
+    Добавил метод для операций с массивом.
+    Исправил объявление массива, до этого ловилось исключение,
+    массив изменился (можно было оставить как есть, если смысл исключительно в отлове исключения).
+    */
+    public static void ArrOp(int[] arr) {
+        try {
+            arr[0] = 1;
+            arr[1] = 2;
+            arr[3] = 9;
+            System.out.println(Arrays.toString(arr));
         } catch (IndexOutOfBoundsException ex) {
             System.out.println("Массив выходит за пределы своего размера!");
         }
     }
-    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
-        System.out.println(a + b);
-    }
-
 }
